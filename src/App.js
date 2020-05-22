93bf9f0eb146970521afa6ac462edd404adfbec1
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-22 20:49:39 +0530

    Installed axios and Added state to App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Player, PlayerList, PlayerForm } from './components/Player'
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col s12">Menu</div>
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {}
+    }
+  }
+
+  updateCurrentPlayer = id => {
+    this.setState({
+      currentPlayer: id
+    })
+  }
+
+  render() {
+    return (
+      <div className="container-fluid" >
+        <div className="row">
+          <div className="col s12">Menu</div>
+        </div>
+        <div className="row">
+          <div className="col s3"><PlayerList /></div>
+          <div className="col s9"><Player /></div>
+        </div>
+        <div className="row">
+          <div className="col s12"><PlayerForm /></div>
+        </div>
       </div>
-      <div className="row">
-        <div className="col s3"><PlayerList /></div>
-        <div className="col s9"><Player /></div>
-      </div>
-      <div className="row">
-        <div className="col s12"><PlayerForm /></div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
